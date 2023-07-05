ff0bf91d4e85d7f6cfbb623e814384e49a8762b4
Niels Gandraß <niels@gandrass.de>
2023-07-05 15:11:43 +0200

    Display log level as string during startup

--- moodle-quiz-archive-worker.py
+++ moodle-quiz-archive-worker.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import logging
 import queue
 import threading
 import time
@@ -172,7 +173,7 @@ def handle_archive_request():
 
 def main():
     app.logger.setLevel(Config.LOG_LEVEL)
-    app.logger.info(f'Running {Config.APP_NAME} version {Config.VERSION} on log level {Config.LOG_LEVEL}')
+    app.logger.info(f'Running {Config.APP_NAME} version {Config.VERSION} on log level {logging.getLevelName(Config.LOG_LEVEL)}')
 
     queue_processing_thread = FlaskThread(target=queue_processing_loop, daemon=True, name='queue_processing_thread')
     queue_processing_thread.start()

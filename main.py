6481a9ca68a8226d6661a322ee4bf9d9cc5fe2c3
Niels Gandraß <niels@gandrass.de>
2024-02-13 11:07:24 +0100

    Simplify main.py and add shebang

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python3
+
+# Moodle Quiz Archive Worker
+# Copyright (C) 2024 Niels Gandraß <niels@gandrass.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from archiveworker import moodle_quiz_archive_worker
 
-
-def main():
-    """
-    Main entry point of the application
-    :return:
-    """
-    moodle_quiz_archive_worker.run()
-
-
 if __name__ == "__main__":
-    main()
+    moodle_quiz_archive_worker.run()

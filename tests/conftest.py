bfe1b3de802044380befafbb16d9b21d6c9de3ef
Niels Gandraß <niels@gandrass.de>
2024-02-13 11:28:37 +0100

    Prepare for pytest and coverage

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+# Moodle Quiz Archive Worker
+# Copyright (C) 2024 Niels Gandraß <niels@gandrass.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+from archiveworker.moodle_quiz_archive_worker import app as original_app
+
+
+@pytest.fixture()
+def app():
+    app = original_app
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()

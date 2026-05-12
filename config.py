3223f2f901b6eb91effa20694049354f58bc5dad
Niels Gandraß <niels@gandrass.de>
2026-05-12 12:24:46 +0200

    Release v4.0.2

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ class Config:
     APP_NAME = "moodle-quiz-archive-worker"
     """Name of this app"""
 
-    VERSION = "4.0.1"
+    VERSION = "4.0.2"
     """Version of this app"""
 
     LOG_LEVEL = logging.getLevelNamesMapping()[parse_env_variable('QUIZ_ARCHIVER_LOG_LEVEL', default='INFO', valtype=str)]
